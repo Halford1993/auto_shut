a9116549f8eae5e46e5a548b8149ef8c175fb528
Halford1993 <82106017+Halford1993@users.noreply.github.com>
2021-04-11 18:20:40 +0200

    GUI 1.0

--- src/auto_shutd/Main.java
+++ src/auto_shutd/Main.java
@@ -7,16 +7,16 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		new Window();
+	}
+	
+	public static void executing(int time) throws Exception {
 		Runtime run = Runtime.getRuntime();
-		System.out.println("Set how long until the shutdown (in minutes): ");
-		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-		int tiempo = Integer.parseInt(br2.readLine())*60;
 		//here we abort previous scheduled shut down (if any)
 		Process pr = run.exec("shutdown -a");
 		pr.waitFor();
-		String cmd = "shutdown -s -t " + tiempo;
+		String cmd = "shutdown -s -t " + time;
 		pr = run.exec(cmd);
 		pr.waitFor();
 	}
-
 }
