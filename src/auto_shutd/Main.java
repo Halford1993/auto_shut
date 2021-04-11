3259417d66451bc8f552ed2f3cd33a2f0834df63
Halford1993 <82106017+Halford1993@users.noreply.github.com>
2021-04-11 17:34:15 +0200

    First version completed

--- src/auto_shutd/Main.java
+++ src/auto_shutd/Main.java
@@ -1,10 +1,22 @@
 package auto_shutd;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		Runtime run = Runtime.getRuntime();
+		System.out.println("Set how long until the shutdown (in minutes): ");
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+		int tiempo = Integer.parseInt(br2.readLine())*60;
+		//here we abort previous scheduled shut down (if any)
+		Process pr = run.exec("shutdown -a");
+		pr.waitFor();
+		String cmd = "shutdown -s -t " + tiempo;
+		pr = run.exec(cmd);
+		pr.waitFor();
 	}
 
 }
